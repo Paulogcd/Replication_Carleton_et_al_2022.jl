cf9bebcfb235f1de04ba7e3a2f99cc07046daec9
Paulogcd <gugelmopaulo@gmail.com>
2025-03-07 11:56:02 +0100

    Trying to add documentation.

---
+++ docs/make.jl
@@ -0,0 +1,5 @@
+using Replication_Carleton_et_al_2022
+using Documenter
+
+makedocs(sitename="My Documentation")
+
--- src/Replication_Carleton_et_al_2022.jl
+++ src/Replication_Carleton_et_al_2022.jl
@@ -2,4 +2,8 @@ module Replication_Carleton_et_al_2022
 
 # Write your package code here.
 
+function test()
+    print("Hello world!")
+end
+
 end
