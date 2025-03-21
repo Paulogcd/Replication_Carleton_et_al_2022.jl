5d61ec80cfb0df67ed540ec4c18f6aad94ee977d
Paulogcd <gugelmopaulo@gmail.com>
2025-03-21 13:26:28 +0100

    testing for figure 1 generation

--- test/2_tests_figure_1.jl
+++ test/2_tests_figure_1.jl
@@ -8,13 +8,14 @@
     # for the data treatment: 
     Replication_Carleton_et_al_2022.load_covar_pop_count()
     Replication_Carleton_et_al_2022.load_coefficients()
+    Replication_Carleton_et_al_2022.load_global_mortality_panel_covariates()
 
     # We then launch the create_figure_1() function, that 
     # generates 3 png files in the 0_output folder.
     Replication_Carleton_et_al_2022.create_figure_1()
     # Finally, we check for their existence:
-    @test isfile("0_output/figure_1_1.png")
-    @test isfile("0_output/figure_1_2.png")
-    @test isfile("0_output/figure_1_3.png")
+    @test isfile(joinpath(pwd(),"0_output/figure_1_1.png"))
+    @test isfile(joinpath(pwd(),"0_output/figure_1_2.png"))
+    @test isfile(joinpath(pwd(),"0_output/figure_1_3.png"))
 
 end
